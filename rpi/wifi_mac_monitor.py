60c9d85427869d173d3d620053ac55794c9babc3
gamename <tennis.n.smith@gmail.com>
2023-08-29 12:08:05 -0500

    working version

--- rpi/wifi_mac_monitor.py
+++ rpi/wifi_mac_monitor.py
@@ -1,10 +1,11 @@
 import subprocess
+import time
 import xml.etree.ElementTree as ET
 
 from statemachine import StateMachine, State
 
 
-def mac_active(net, mac):
+def mac_status(net, mac):
     process = subprocess.Popen(['sudo', 'nmap', '-oX', '/tmp/nmap.xml', '-sn', net],
                                stdout=subprocess.PIPE)
     process.wait()
@@ -12,8 +13,8 @@ def mac_active(net, mac):
     tree = ET.parse('/tmp/nmap.xml')
     for node in tree.iter('address'):
         if node.attrib['addrtype'] == 'mac' and node.attrib['addr'] == mac:
-            return True
-    return False
+            return 'present'
+    return 'absent'
 
 
 class MacAddressMonitoringMachine(StateMachine):
@@ -27,11 +28,22 @@ class MacAddressMonitoringMachine(StateMachine):
 
 
 network_address = '192.168.0.1/24'
-mac_address = '28:CD:C1:04:80:97'
+# mac_address = '28:CD:C1:04:80:97'
+mac_address = '28:CD:C1:04:7F:69'
 
 sm = MacAddressMonitoringMachine()
 
-if not mac_active(network_address, mac_address):
+if mac_status(network_address, mac_address) != 'present':
     sm.send('cycle')
 
 print(sm.current_state.id)
+
+current = sm.current_state.id
+
+while True:
+    sample = mac_status(network_address, mac_address)
+    if sample != current:
+        print(f'{current}->{sample}')
+        sm.send('cycle')
+        current = sample
+    time.sleep(45)
