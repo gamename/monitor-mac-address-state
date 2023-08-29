2441167177202171a9d409063f97a6dbe36a18f7
gamename <tennis.n.smith@gmail.com>
2023-08-29 04:10:01 -0500

    add state class

--- rpi/wifi_mac_monitor.py
+++ rpi/wifi_mac_monitor.py
@@ -1,10 +1,23 @@
 import subprocess
 import xml.etree.ElementTree as ET
+
 from statemachine import StateMachine, State
 
 
+def mac_active(net, mac):
+    process = subprocess.Popen(['sudo', 'nmap', '-oX', '/tmp/nmap.xml', '-sn', net],
+                               stdout=subprocess.PIPE)
+    process.wait()
+
+    tree = ET.parse('/tmp/nmap.xml')
+    for node in tree.iter('address'):
+        if node.attrib['addrtype'] == 'mac' and node.attrib['addr'] == mac:
+            return True
+    return False
+
+
 class MacAddressMonitoringMachine(StateMachine):
-    present = State()
+    present = State(initial=True)
     absent = State()
     cycle = (present.to(absent) | absent.to(present))
 
@@ -16,11 +29,9 @@ class MacAddressMonitoringMachine(StateMachine):
 network_address = '192.168.0.1/24'
 mac_address = '28:CD:C1:04:80:97'
 
-process = subprocess.Popen(['sudo', 'nmap', '-oX', '/tmp/nmap.xml', '-sn', network_address],
-                           stdout=subprocess.PIPE)
-process.wait()
+sm = MacAddressMonitoringMachine()
 
-tree = ET.parse('/tmp/nmap.xml')
-for node in tree.iter('address'):
-    if node.attrib['addrtype'] == 'mac' and node.attrib['addr'] == mac_address:
-        print(node.attrib['addr'])
+if not mac_active(network_address, mac_address):
+    sm.send('cycle')
+
+print(sm.current_state.id)
